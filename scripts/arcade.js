c08dce1a67fc0b39d891e3dfeab852ac53eea475
brazeenNP <s10255790@connect.np.edu.sg>
2024-05-31 11:24:04 +0800

    random buildings generated every pg refresh but user cant choose and build

--- scripts/arcade.js
+++ scripts/arcade.js
@@ -23,6 +23,24 @@ function placeBuilding(type, x, y){
     spot.classList.add(type)
 }
 
+function generateRandomBuilding(){
+    //list of png names
+    let typeList = ["commercial", "industry", "residential", "park", "road"]
+    let choice1 = Math.floor(Math.random() * 5)
+    let choice2 = Math.floor(Math.random() * 5)
+    while (choice2 == choice1 ) {
+        //if choice is the same, change one to make it different
+        choice2 = Math.floor(Math.random() * 5)
+    }
+    choice1 = typeList[choice1]
+    choice2 = typeList[choice2]
+    const randomdiv1 = document.getElementById('randombuilding-1')
+    const randomdiv2 = document.getElementById('randombuilding-2')
+    randomdiv1.innerHTML = `<img src="./assets/${choice1}.png" width="100%"></img>`
+    randomdiv2.innerHTML = `<img src="./assets/${choice2}.png" width="100%"></img>`
+}
+
+generateRandomBuilding()
 placeBuilding("road",1,1)
 placeBuilding("road",1,2)
 placeBuilding("road",1,3)
